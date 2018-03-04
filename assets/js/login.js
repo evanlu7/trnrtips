931098c7d90a7bd6439c5dd9e19c1ea38be0bb59
Evan Lu <evanlu7@gmail.com>
2018-03-03 17:17:05 -0800

    cleaned up js and added social animation

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).ready(function(){       
             var scroll_pos = 0;
             $(document).scroll(function() { 
@@ -11,6 +8,18 @@ $(document).ready(function(){
                 } else {
                     $("nav").css('background-color', 'transparent');
                     $("nav a").css('color', 'white');
+                }
+            });
+});
+
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 160) {
+                    $(".social").addClass("animated bounceIn");
+                } else {
+                  $(".social").addClass("");
                 }
             });
 });
